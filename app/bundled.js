d42d570e93839cb1ed9b83140056bd4508e7a371
Mirko <mg@net-insiders.com>
2022-09-08 12:38:32 +0200

    Weiter gehts

--- app/bundled.js
+++ app/bundled.js
@@ -15,7 +15,7 @@
   \***********************************/
 /***/ (() => {
 
-eval("alert(\"Hello, this is just a test!!!\");\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
+eval("alert(\"Hello! I am an alert box!!\");\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
 
 /***/ })
 
